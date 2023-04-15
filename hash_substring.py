22f4cde3229e55e8a00fce5db549ade690edc21f
annijara <99959888+annijara@users.noreply.github.com>
2023-04-15 17:11:32 +0300

    something

--- hash_substring.py
+++ hash_substring.py
@@ -3,7 +3,18 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
+    modee = input()
+    if "I" in modee:
+        pattern = input().rstrip()
+        text = input().rstrip()
+
+    elif "F" in modee:
+        num = "06"
+        with open("./tests/"+ num, mode="r") as fails:
+            txt = fails.read()
+            x = txt.splitlines()
+            pattern = x[0].rstrip()
+            text = x[1].rstrip()      
     
     # after input type choice
     # read two lines 
@@ -11,9 +22,7 @@ def read_input():
     # second line is text in which to look for pattern 
     
     # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return (pattern, text)
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,9 +30,38 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
+    main_text_len = len(text)
+    pattern_len = len(pattern)
+    d = 256
+    q = 13
+    h = 1
+    p = 0
+    t = 0
+    
+    for i in range (pattern_len -1):
+        h = (h * d) % q
+        
+    for i in range(pattern_len):
+        p = (d*p + ord(pattern[i])) % q
+        t = (d*t + ord(text[i])) % q
+        
+    for i in range(main_text_len - pattern_len + 1):
+        if p == t:
+            for j in range(pattern_len):
+                if text[i+j] != pattern[j]:
+                    break
+            j += 1
+            if j == pattern_len:
+                return [i]
+        
+        if i < main_text_len - pattern_len:
+            t = (d*(t-ord(text[i])*h) + ord(text[i+pattern_len])) % q
+            
+            if t < 0:
+                t = t+q
+    
     # and return an iterable variable
-    return [0]
+    
 
 
 # this part launches the functions
